1d37879fd607d4330dd9a2c9f68f6f09424244a5
eongyu <kegryan@nate.com>
2025-05-26 15:47:54 +0900

    Add files via upload

---
+++ sketch_bot_description/launch/display.launch.py
@@ -0,0 +1,37 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    urdf_path = PathJoinSubstitution([
+        FindPackageShare('sketch_bot_description'),
+        'urdf',
+        'sketch_bot.urdf.xacro'
+    ])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro', urdf_path])
+                'robot_description': Command(['python3', '-m', 'xacro', urdf_path])
+            }]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
